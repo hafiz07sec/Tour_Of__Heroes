2faf78e314d49273c869c7d056d0d57e0294287d
Hafizur Rahman Masum <32071354+hafiz07sec@users.noreply.github.com>
2022-06-19 12:13:24 +0200

    Router added in the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+ 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
